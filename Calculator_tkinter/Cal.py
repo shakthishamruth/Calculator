3ac4d57c9314a5b97f9d8e509feeb02b162961ce
Shakthi <shakthishamruth@gmail.com>
2022-07-01 18:45:05 +0530

    Updated Cal.py

--- Calculator_tkinter/Cal.py
+++ Calculator_tkinter/Cal.py
@@ -9,14 +9,20 @@ e = Entry(win, width=41, borderwidth=10, font=500)
 e.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
 eq = e.get()
 
+input_state = True
+
 
 # funcs
 def button_click(num):
-    global eq
+    global eq, input_state
+    if not input_state and num.isdigit():
+        e.delete(0, END)
+        eq = e.get()
     current = e.get()
     e.delete(0, END)
     e.insert(0, str(current) + str(num))
     eq = e.get()
+    input_state = True
 
 
 def button_clr():
@@ -26,41 +32,51 @@ def button_clr():
 
 
 def button_backspace():
-    global eq
-    current = e.get()
-    e.delete(0, END)
-    e.insert(0, str(current)[:-1])
-    eq = e.get()
+    global eq, input_state
+    if input_state:
+        current = e.get()
+        e.delete(0, END)
+        e.insert(0, str(current)[:-1])
+        eq = e.get()
+    else:
+        e.delete(0, END)
+        eq = e.get()
 
 
 def button_eq():
-    global eq
+    global eq, input_state
     e.delete(0, END)
-    e.insert(0, str(eval(eq)))
-    eq = e.get()
+    try:
+        soln = str(eval(eq))
+        e.insert(0, soln)
+        eq = e.get()
+    except:
+        e.insert(0, "ERROR")
+        eq = e.get()
+    input_state = False
 
 
 def button_sq():
     global eq
     current = e.get()
     e.delete(0, END)
-    e.insert(0, str(eval(str(current) + '*' + str(current))))
+    e.insert(0, str(eval(str(eval(current)) + '*' + str(eval(current)))))
     eq = e.get()
 
 
 # Define buttons
 button_backs = Button(win, text="← ", padx=15, pady=0, command=button_backspace, fg="#FF0000", font=500, borderwidth=2)
 
-button_1 = Button(win, text="1", padx=42, pady=20, command=lambda: button_click(1), bg="#D9D9D9", font=500)
-button_2 = Button(win, text="2", padx=42, pady=20, command=lambda: button_click(2), bg="#D9D9D9", font=500)
-button_3 = Button(win, text="3", padx=42, pady=20, command=lambda: button_click(3), bg="#D9D9D9", font=500)
-button_4 = Button(win, text="4", padx=42, pady=20, command=lambda: button_click(4), bg="#D9D9D9", font=500)
-button_5 = Button(win, text="5", padx=42, pady=20, command=lambda: button_click(5), bg="#D9D9D9", font=500)
-button_6 = Button(win, text="6", padx=42, pady=20, command=lambda: button_click(6), bg="#D9D9D9", font=500)
-button_7 = Button(win, text="7", padx=42, pady=20, command=lambda: button_click(7), bg="#D9D9D9", font=500)
-button_8 = Button(win, text="8", padx=42, pady=20, command=lambda: button_click(8), bg="#D9D9D9", font=500)
-button_9 = Button(win, text="9", padx=42, pady=20, command=lambda: button_click(9), bg="#D9D9D9", font=500)
-button_0 = Button(win, text="0", padx=42, pady=20, command=lambda: button_click(0), bg="#D9D9D9", font=500)
+button_1 = Button(win, text="1", padx=42, pady=20, command=lambda: button_click("1"), bg="#D9D9D9", font=500)
+button_2 = Button(win, text="2", padx=42, pady=20, command=lambda: button_click("2"), bg="#D9D9D9", font=500)
+button_3 = Button(win, text="3", padx=42, pady=20, command=lambda: button_click("3"), bg="#D9D9D9", font=500)
+button_4 = Button(win, text="4", padx=42, pady=20, command=lambda: button_click("4"), bg="#D9D9D9", font=500)
+button_5 = Button(win, text="5", padx=42, pady=20, command=lambda: button_click("5"), bg="#D9D9D9", font=500)
+button_6 = Button(win, text="6", padx=42, pady=20, command=lambda: button_click("6"), bg="#D9D9D9", font=500)
+button_7 = Button(win, text="7", padx=42, pady=20, command=lambda: button_click("7"), bg="#D9D9D9", font=500)
+button_8 = Button(win, text="8", padx=42, pady=20, command=lambda: button_click("8"), bg="#D9D9D9", font=500)
+button_9 = Button(win, text="9", padx=42, pady=20, command=lambda: button_click("9"), bg="#D9D9D9", font=500)
+button_0 = Button(win, text="0", padx=42, pady=20, command=lambda: button_click("0"), bg="#D9D9D9", font=500)
 
 button_sqr = Button(win, text='^2', padx=38, pady=20, command=button_sq, font=500)
 button_dot = Button(win, text='‧', padx=42, pady=20, command=lambda: button_click('.'), font=500)
@@ -89,9 +105,9 @@ button_8.grid(row=2, column=1)
 button_9.grid(row=2, column=2)
 button_mul.grid(row=2, column=3)
 
-button_0.grid(row=5, column=0)
+button_0.grid(row=5, column=1)
 
-button_dot.grid(row=5, column=1)
+button_dot.grid(row=5, column=0)
 button_equals.grid(row=5, column=2, columnspan=2)
 button_sqr.grid(row=1, column=2)
 button_clr.grid(row=1, column=0, columnspan=2)
